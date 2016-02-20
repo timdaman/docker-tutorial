4d5bc4b8a46b47fe752dede717352fcca8ecc87a
Tim Laurence <timdaman@gmail.com>
2016-02-20 18:42:28 -0500

    Updated setup to use docker-machine which is easier to use and smaller than vagrant based setup

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # docker-tutorial
-A environment and series of examples for help people learn about docker.
+A environment and series of examples for learn about docker.
+
+# To participate in the tutorial you need
+1. a laptop with
+  * 1-2GB of free RAM
+  * 5GB of free disk space in your home directory
+1. To have performed the setup steps below.
+
+
+# You *MUST* do the following before you arrive to your tutorial session
+* Install Virtual Box if you don't already have it<br>
+You can get there here
+      https://www.virtualbox.org/wiki/Downloads
+* Install docker-machine<br>
+You ONLY need docker-machine, not the completion scripts.
+You find the installation steps for your platform here.
+      https://docs.docker.com/machine/install-machine/
+* Run the following commands to prepare your laptop
+      docker-machine create --driver virtualbox tutorial
+      docker-machine ssh tutorial curl --silent -o /tmp/setup_lab.sh -L  https://raw.githubusercontent.com/timdaman/docker-tutorial/master/setup_lab.sh
+      docker-machine ssh tutorial /tmp/setup_lab.sh
+
+The reason why I asking you to do this work ahead of time is because
+* I am going to dive straight into using Docker rather than wasting a lot of time showing how to install it. If you are not prepared you will fall behind.
+* In a busy class if everyone tried to download the needed files for the example there is a high risk that the network would collapse under the combined demand. These prep steps caches almost all the data you need to follow along.
+* This give you a chance to deal with any installation problems before coming to tutorial.
--- setup_lab.sh
+++ setup_lab.sh
@@ -1,65 +1,11 @@
-#!/bin/bash
-#
-# Try to install vagrant and virtualbox environemnt for learning about
-# docker
-#
+#!/bin/sh
 
-which VBoxManage > /dev/null 2>&1
-VIRTUALBOX_PRESENT=$?
-which vagrant > /dev/null 2>&1
-VAGRANT_PRESENT=$?
+set -x
 
-SOMETHING_MISSING=0
-if [ $VIRTUALBOX_PRESENT ]
-then
-  echo "Found VirtualBox!"
-else
-  echo "VirtualBox is not found in path, I am going to try to install it?"
-  SOMETHING_MISSING=1
-fi
-
-if [ $VAGRANT_PRESENT ]
-then
-  echo "Found Vagrant!"
-else
-  echo "Vagrant is not found in path, I am going tot ry to install it"
-  SOMETHING_MISSING=1
-fi
-
-if [ $SOMETHING_MISSING -eq 1 ]
-then
-  ARCH=`uname -m`
-  echo "I am going to try to install vagrant and virtualbox."
-  if [ -f /etc/debian_version ]; then
-      echo "Looks like a debian based distro"
-      apt-get install vagrant virtualbox
-      INSTALL_RESULTS = $?
-  elif [ -f /etc/redhat-release ]; then
-      echo "Looks like a RedHat based distro"
-      yum install vagrant virtualbox
-      INSTALL_RESULTS = $?
-  else
-      echo "Don't know your distro."
-      INSTALL_RESULTS=1
-  fi
-  if [ $INSTALL_RESULTS -ne 0 ]
-  then
-    echo <<END
-You will need to manully install the right packages from here.
-https://www.vagrantup.com/downloads.html
-https://www.virtualbox.org/wiki/Downloads
-END
-    exit 1
-  fi
-fi
-
-
-#wget $VAGRANTFILE
-echo "Hang on!!! This going to take a while, esepcially if you have a slow internet connection."
-sleep 5
-vagrant up && vagrant suspend
-VARGRANT_RC=$?
-
-[ $VARGRANT_RC -ne 0 ] && echo "Something went wrong, sorry" && exit 1
+docker pull mysql:5.7 &&\
+docker pull php:5.6 &&\
+sudo curl  --silent -o /usr/local/bin/docker-compose -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-Linux-x86_64 &&\
+sudo chmod +x /usr/local/bin/docker-compose &&\
+curl --silent -o wordpress.tar.gz https://wordpress.org/latest.tar.gz
 
 echo "Congratulations, you are ready to learn about docker now"
