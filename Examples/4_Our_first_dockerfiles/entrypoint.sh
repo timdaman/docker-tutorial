9e1634575659e4b200d235d32c69525c7ef01f20
Tim Laurence <timdaman@gmail.com>
2016-03-23 23:51:30 -0400

    Be gone typos Fixed ugliness Fixed bug that resulted in example 6 not running.

--- Examples/4_Our_first_dockerfiles/entrypoint.sh
+++ Examples/4_Our_first_dockerfiles/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Running at `date`" >> run.timestamp
 echo "I see the following ocnfiguration was passed in"
 echo
-echo "CONFIGURTAION=$CONFIGURTAION"
+echo "CONFIGURATION=$CONFIGURATION"
 echo
 cat /*.timestamp
 echo "Hello world" | /nc -l -p 42
--- Examples/6_Bring_it_all_together/todo/app/entrypoint.sh
+++ Examples/6_Bring_it_all_together/todo/app/entrypoint.sh
@@ -7,7 +7,7 @@ RAILS_ENV=$ENVIRONMENT
 sed -i -e "s/MYSQL_DATABASE/$MYSQL_DATABASE/" \
        -e "s/MYSQL_HOST/$MYSQL_HOST/" \
        -e "s/MYSQL_USER/$MYSQL_USER/" \
-       -e "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/"
+       -e "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/" config/database.yml
 
 rake db:migrate
 
